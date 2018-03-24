4685acafae117f7a8b9a3424ab659c1cef9e29ef
ravirocx <itsyourraviraj@gmail.com>
2018-03-24 15:42:42 +0530

    test cases rectified

---
+++ lib/github/client.ex
@@ -0,0 +1,44 @@
+defmodule Github.Client do
+  alias Github.Client
+
+  defstruct [:base_url, :ssl_options]
+
+  def get_plain(%Client{} = client, url, params \\ []) do
+    get(client, url(client, url, params), [{"accept", "text/plain"}], &(&1))
+  end
+
+  def get_json(%Client{} = client, url, params \\ []) do
+    get(client, url(client, url, params), [{"accept", "application/json"}], &Antidote.decode/1)
+  end
+
+  def get_yaml(%Client{} = client, url, params \\ []) do
+    get(client, url(client, url, params), [{"accept", "application/yaml"}], &decode_yaml/1)
+  end
+
+  @doc false
+  # Public for testing purposes
+  def decode_yaml(content) do
+    case :yamerl.decode(content, [:str_node_as_binary, map_node_format: :map]) do
+      [parsed] -> {:ok, parsed}
+      _ -> {:error, :invalid}
+    end
+  end
+
+  defp get(client, url, headers, cb) do
+    ssl_options = client.ssl_options
+    options = [:with_body, ssl_options: ssl_options]
+
+    case :hackney.get(url, headers, <<>>, options) do
+      {:ok, 200, _headers, data} ->
+        cb.(data)
+      {:ok, status, _headers, data} ->
+        {:error, {status, data}}
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  defp url(%{base_url: base}, url, params) do
+    Path.join(base, url) <> "?" <> URI.encode_query(params)
+  end
+end
--- test/runner/config_test.exs
+++ test/runner/config_test.exs
@@ -1,22 +1,23 @@
 defmodule ElixirBench.Runner.ConfigTest do
   use ExUnit.Case, async: true
-  import ElixirBench.Runner.Config
+  
 
-  @repo_slug_fixture "elixir-ecto/ecto"
+  @repo_owner "elixir-ecto"
+  @repo_name "ecto"
   @branch_fixture "mm/benches"
   @commit_fixture "207b2a0fb5407b7162a454a12bacf8f1a4c962c0"
 
   @tag requires_internet_connection: true
   describe "fetch_config_by_repo_slug/1" do
     test "loads configuration from repo" do
-      assert {:ok, %ElixirBench.Runner.Config{}} = fetch_config_by_repo_slug(@repo_slug_fixture, @branch_fixture)
-      assert {:ok, %ElixirBench.Runner.Config{}} = fetch_config_by_repo_slug(@repo_slug_fixture, @commit_fixture)
+      assert {:ok, _} = Github.fetch_config(@repo_owner, @repo_name, @branch_fixture)
+      assert {:ok, _} = Github.fetch_config(@repo_owner, @repo_name, @commit_fixture)
     end
 
     test "returns error when file does not exist" do
-      assert fetch_config_by_repo_slug("elixir-ecto/ecto", "not_a_branch") == {:error, :config_not_found}
-      assert fetch_config_by_repo_slug("not-elixir-ecto/ecto", "mm/benches") == {:error, :config_not_found}
-      assert fetch_config_by_repo_slug("not-elixir-ecto/ecto") == {:error, :config_not_found}
+      assert Github.fetch_config("elixir-ecto", "ecto", "not_a_branch") == {:error, :failed_config_fetch}
+      assert Github.fetch_config("not-elixir", "ecto/ecto", "mm/benches") == {:error, :failed_config_fetch}
+      assert Github.fetch_config("not-elixir", "-ecto", "/ecto") == {:error, :failed_config_fetch}
     end
   end
 end
